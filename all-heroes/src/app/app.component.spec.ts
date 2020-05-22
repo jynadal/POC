6f36435bfb728100890e8d6556c189af5cc21bec
jynadal <nadaljeanyves@hotmail.fr>
2020-05-22 11:37:03 +0200

    Begins Detail-Hero

--- all-heroes/src/app/app.component.spec.ts
+++ all-heroes/src/app/app.component.spec.ts
@@ -25,6 +25,9 @@ describe('AppComponent', () => {
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
+
+
+    
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('.content span').textContent).toContain('all-heroes app is running!');
   });
--- all-heroes/src/app/app.module.ts
+++ all-heroes/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { MemoCPComponent } from './memo-cp/memo-cp.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    MemoCPComponent
+    MemoCPComponent,
+    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
